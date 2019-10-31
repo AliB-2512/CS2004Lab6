c282072fa60f094b9482f183a4f0c33cb91b8100
Ali Bachar <alibachar@icloud.com>
2019-10-31 23:34:44 +0000

    6.3 Exercise 2

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# CS2004Lab6
+# CS2004Lab6 - Sorting Algorithms
+
+Worksheet 6 for module CS2004 Algorithms and their appplications
+
+- [x] 6.2 Exercise 1  
+- [x] 6.3 Exercise 2  
+
+
+
+- [x] Final Push to GitHub :clap: :clap:
+
+
--- src/Lab6.java
+++ src/Lab6.java
@@ -1,4 +1,44 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 
 public class Lab6 {
+	public static void main(String args[]) {
+		int n = 25000;
+		ArrayList<Integer> x = RandomArray(n);
+		System.out.println("Unsorted Array:");
+		ShowArray(x);
+		System.out.println("Sort A:");
+		ShowArray(ThreeSorts.SortA(x));
+		System.out.println("Sort B:");
+		ShowArray(ThreeSorts.SortB(x));
+		System.out.println("Sort C:");
+		ShowArray(ThreeSorts.SortC(x));
+	}
+	public static int randomNum() {
+		Random rand = new Random();
+		rand.setSeed(System.currentTimeMillis());
+		Integer r = Math.abs(rand.nextInt() % 256);
+		return r;
+	}
+	public static ArrayList<Integer> RandomArray(int n) {
+		ArrayList<Integer> arrayRandom = new ArrayList<Integer>();
+		for (int i = 0; i <= n-1;i++) {
+			int value = randomNum();
+			arrayRandom.add(i,value);
+			try {
+	              
+                Thread.sleep(1);
 
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+		}
+		
+		return arrayRandom;
+	}
+	public static void ShowArray(ArrayList<Integer> x) {
+			System.out.println(x);
+		}
+		
 }
